da04b744fca18ab0d3a389d53ca58815dd9c449f
maokangren <767709667@qq.com>
2018-07-24 19:54:15 +0800

    bspatch init

---
+++ library-bspatch/src/main/java/com/smart/library/util/bspatch/BSPatchUtil.kt
@@ -0,0 +1,15 @@
+package com.smart.library.util.bspatch
+
+object BSPatchUtil {
+    /**
+     * @param basePath      基础文件
+     * @param syntheticPath 合成的目标文件
+     * @param patchPath     合成所需要的差分文件
+     * @return main 返回值
+     */
+    external fun bspatch(basePath: String, syntheticPath: String, patchPath: String): Int
+
+    init {
+        System.loadLibrary("bspatch")
+    }
+}
